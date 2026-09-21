e5165055b7aa3cceea764a879daadcfea6017d3b
shxmk11 <20252213@sungshin.ac.kr>
2026-09-21 22:50:22 +0900

    Add files via upload

---
+++ Josephus.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int K;
+    cin >> K;
+
+    queue <int> q;
+
+    for (int i = 1; i <= N; i++)
+    {
+        q.push(i);
+    }
+
+    while (q.size() > 1) {
+        for (int i = 0; i < (K - 1); i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+
+        cout << q.front() << endl;
+        q.pop();
+    }
+
+    cout << q.front() << endl;
+}
